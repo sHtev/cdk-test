c2cd696fe160c43d3a3412489a48c100deaaa3d8
Stephen Gilroy <stephen.gilroy@isotoma.com>
2023-01-10 10:14:10 +0000

    initial pipeline details

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CdkTest',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('sHtev/cdk-test', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
